f656fcd067aa80fcc1e0aaf1a6e015ccac08dfc8
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-08-26 20:22:44 -0300

    Arreglo en tamaño de video

--- src/components/Inicio/SectionPresentacion/VideoPresentacion.jsx
+++ src/components/Inicio/SectionPresentacion/VideoPresentacion.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoPresentacion = () => {
     return (
         <div className='absolute top-0 left-0 -z-20 w-full h-full overflow-hidden'>
-            <video src='./images/video-inicio.mp4' autoPlay muted loop />
+            <video src='./images/video-inicio.mp4' className='w-full h-full object-cover' autoPlay muted loop />
         </div>
     )
 }
